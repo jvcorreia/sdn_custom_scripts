562fedf26130dd17780924872ca5466cb2d3e17c
jvcorreia <joaovidalcorreia@ua.pt>
2020-11-05 12:01:57 +0000

    -Changed the port of the controller from the environment variable pOF to the port 6633, thus the script no longer depends on setting that environment variable.
    -The script now echoes the command, to check if it is the intended command

--- mn_onos_cluster.sh
+++ mn_onos_cluster.sh
@@ -4,14 +4,14 @@
 #use as follows: mn_onos <name of onos 1 container> ... <name of onos n container> --topo <topology>
 #example: mn_onos
 
-
+ctrlPort=6633
 comd="sudo mn --mac --switch=ovs,protocols=OpenFlow14 "
 for(( arg=1; arg <= "$#"; arg++))
 do
 	if [[ "${!arg}" == "onos"* ]]
 	then
 		ipAddr=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "${!arg}")
-		ctrlr="--controller=remote,ip=$ipAddr,port=$pOF "
+		ctrlr="--controller=remote,ip=$ipAddr,port=$ctrlPort "
 		comd="$comd$ctrlr"
 	elif [[ "${!arg}" == "--topo" ]]
 	then
@@ -20,4 +20,5 @@ do
 		comd="$comd--topo $topo"
 	fi		
 done
+echo "$comd"
 eval "$comd"
